5f6db07f02ebd7b4b86b27f0c1b7455722434388
Anastasia Canlas <recr38@gmail.com>
2019-11-11 12:19:27 -0800

    Completed Practice Drills01

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,46 @@
 
-function sumArray(){
-	
+function sumArray(array){
+  var result = null;
+  for(var i = 0; i < array.length; i++){
+    result += array[i];
+  }
+  return result;
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(array, target_sum){
+  var result = [];
+  var summed_num = null;
+  for(var i = 0; i < array.length; i++){
+    if(summed_num + array[i] <= target_sum){
+      summed_num += array[i];
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan(array, num){
+  var result = [];
+  for(var i = 0; i < array.length; i++){
+    var index_value = array[i];
+    if(index_value.length < num){
+      result.push(index_value);
+    }
+  }
+  return result;
 }
 
-function makeLabel(){
+function makeLabel(person){
+  var result = null;
 
+  result = person['greeting'] + " ";
+  result+= person['givenName'] + " ";
+  result+= person['familyName'] + "\n";
+  result+= person['home address']['streetNumber'] + " ";
+  result+= person['home address']['streetName'] + "\n";
+  result+= person['home address']['city'] + ", ";
+  result+= person['home address']['state'] + " ";
+  result+= person['home address']['zip'];
+
+  return result;
 }
